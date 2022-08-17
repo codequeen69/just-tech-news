d4375cbb72f1cbd76a3bbd6d572ad6e9518ae8d3
Venus DM <crhowe90@gmail.com>
2022-08-17 15:43:40 -0500

    votes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 //saying a user can have many posts
 User.hasMany(Post,{
@@ -8,10 +9,37 @@ User.hasMany(Post,{
 //saying post can only belong to one user
 Post.belongsTo(User, {
     foreignKey: 'user_id'
-})
+});
 
+//these belongsToMany allow the User and Post to query each other's information in the context of a vote.
+//we can now see which users voted on a single post and which posts a single user voted on.
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
 
 
 
 //why do we sometimes have to use curly brackets?
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
