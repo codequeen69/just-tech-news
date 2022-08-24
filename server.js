6b4afac72fcfdaf6943cad01d12752dd1b75b1cc
Venus DM <crhowe90@gmail.com>
2022-08-24 16:32:14 -0500

    refactor with partials and tests

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -22,7 +23,9 @@ const sess ={
 };
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 //will look for an index.js file
 const routes = require('./controllers');
